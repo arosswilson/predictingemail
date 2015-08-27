851c19600ffe059860bc3bcec3983fa42af9a6da
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-08-26 21:54:45 -0400

    dry post route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,22 +1,11 @@
-require 'pry'
 require_relative '../models/user'
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
 post '/emailprediction' do
-  @emailprediction_hash = Hash.new
-  @emailprediction_hash[:patterns] = []
-  @emailprediction_hash[:name] = params['user']['name']
-  @emailprediction_hash[:company_url] = params['user']['company_url']
-  pattern_array = User.find_patterns(params['user']['company_url']).uniq
-  @emailprediction_hash[:single_pattern] = true if pattern_array[0] != nil && pattern_array.length == 1
-  pattern_array.each do |pattern|
-    @emailprediction_hash[:patterns] << create_predicted_email(pattern, params['user']['name'], params['user']['company_url'])
-  end
+  @predict_hash = { name: params['user']['name'], url: params['user']['url'] }
+  pattern_array = User.find_patterns(params['user']['url']).uniq
+  collect_predicted_emails(pattern_array, @predict_hash)
   erb :emailprediction
-
-
-
 end
