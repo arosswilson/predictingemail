d1de2b4c00ce6d9574b7139882a19732e04f3e1e
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-08-28 13:23:29 -0400

    add if statement for ajax

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,5 +7,9 @@ post '/emailprediction' do
   @predict_hash = { name: params['user']['name'], url: params['user']['url'] }
   pattern_array = User.find_patterns(params['user']['url'])
   collect_predicted_emails(pattern_array, @predict_hash)
-  erb :emailprediction
+  if request.xhr?
+    erb :emailprediction, layout: false
+  else
+    erb :emailprediction
+  end
 end
