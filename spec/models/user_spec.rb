b2684dc4c63eeca9fb3f52a21f270b4e3e50328b
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-08-28 10:11:25 -0400

    add model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,59 +1,44 @@
 require 'spec_helper'
+require_relative '../../app/helpers/predicting_email'
 
 describe User do
   describe 'find unique patters based on company url' do
+    before :each do
+      john = User.create(
+        name: 'John Ferguson',
+        email: 'john.ferguson@alphasights.com',
+        company_url: 'alphasights.com',
+        email_pattern: 'first_name_dot_last_name'
+        )
+      ross = User.create(
+        name: 'Ross Wilson',
+        email: 'r.w@alphasights.com',
+        company_url: 'alphasights.com',
+        email_pattern: 'first_initial_dot_last_initial'
+        )
+      ron = User.create(
+        name: 'Ron Winston',
+        email: 'ron.winston@alphasights.com',
+        company_url: 'alphasights.com',
+        email_pattern: 'first_name_dot_last_name'
+        )
+      russ = User.create(
+        name: 'Russ Wilkins',
+        company_url: 'google.com'
+        )
+    end
     context 'should find patterns' do
       it "should return an array of unique patterns based on company url" do
-        john = User.create(
-          name: 'John Ferguson',
-          email: 'john.ferguson@alphasights.com',
-          company_url: 'alphasights.com',
-          email_pattern: 'first_name_dot_last_name'
-          )
-        ross = User.create(
-          name: 'Ross Wilson',
-          email: 'r.w@alphasights.com',
-          company_url: 'alphasights.com',
-          email_pattern: 'first_initial_dot_last_initial'
-          )
-        ron = User.create(
-          name: 'Ron Winston',
-          email: 'ron.winston@alphasights.com',
-          company_url: 'alphasights.com',
-          email_pattern: 'first_name_dot_last_name'
-          )
         expect(User.find_patterns('alphasights.com')).to eq ['first_name_dot_last_name', 'first_initial_dot_last_initial']
       end
     end
     context 'should return nil' do
       it "for a company url that no user has" do
-        john = User.create(
-          name: 'John Ferguson',
-          email: 'john.ferguson@alphasights.com',
-          company_url: 'alphasights.com',
-          email_pattern: 'first_name_dot_last_name'
-          )
-        ross = User.create(
-          name: 'Ross Wilson',
-          email: 'r.w@alphasights.com',
-          company_url: 'alphasights.com',
-          email_pattern: 'first_initial_dot_last_initial'
-          )
         expect(User.find_patterns('unc.edu')).to eq [nil]
       end
 
       it "for user that have a company url but no pattern" do
-        john = User.create(
-          name: 'John Ferguson',
-          email: 'john.ferguson@alphasights.com',
-          company_url: 'alphasights.com'
-          )
-        ross = User.create(
-          name: 'Ross Wilson',
-          email: 'r.w@alphasights.com',
-          company_url: 'alphasights.com'
-          )
-        expect(User.find_patterns('alphasights.com')).to eq [nil]
+        expect(User.find_patterns('google.com')).to eq [nil]
       end
     end
   end
